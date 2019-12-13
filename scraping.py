473f1c9e189d34d24f854a9a57f8a998c8648d71
Jennifer Tian <jennifertian@CVRILooeyiMac02.ucsf.edu>
2019-12-12 17:24:32 -0800

    debugging

--- scraping.py
+++ scraping.py
@@ -6,9 +6,17 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
 def scrape_all():
-   # Initiate headless driver for deployment
-   browser = Browser("chrome", executable_path="chromedriver", headless=True)
-   news_title, news_paragraph = mars_news(browser)
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
 
 executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
 browser = Browser('chrome', **executable_path)
@@ -79,10 +87,10 @@ def mars_facts():
     df.set_index('Description', inplace=True)
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
 #end automatic browser
+browser.quit()
 
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
-
-browser.quit()
